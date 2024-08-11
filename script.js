8830ff91c51b7f946ef8b92e763f7ab93f7fa6af
Ulyt1703 <138807803+Ulyt1703@users.noreply.github.com>
2024-08-11 02:59:42 +0200

    D__push-11.08.2024-02:59

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let userList = document.getElementById('userList')
 userList.addEventListener('click', deleteUser)
 
 function deleteUser(event) {
-    if(event.target.nodeName !== "BUTTON"){
-        return
+    if(event.target.nodeName === "BUTTON"){
+        if(event.target.classList = 'deleteButton') {
+            let userItem = event.target.closest('li')
+            userItem.remove()
+        }
     }
-    if(event.target.classList = 'deleteButton') {
-        let userItem = event.target.closest('li')
-        userItem.remove()
+    else{
+        return
     }
 }
